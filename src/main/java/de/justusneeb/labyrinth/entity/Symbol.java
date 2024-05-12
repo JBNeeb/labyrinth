d69cc155840e210db7335f8299fc1c41cf3d218c
jnbea <justus.neeb@justusbneeb.de>
2024-05-12 16:14:39 +0200

    picture renamed new enum class symbol created updated enum class to upper case

---
+++ src/main/java/de/justusneeb/labyrinth/entity/Symbol.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.justusneeb.labyrinth.entity;
+
+/**
+ *
+ * @author jnbea
+ */
+public enum Symbol {
+
+    // on board
+    SKULL,
+    SWORD,
+    BAG_OF_GOLD,
+    KEYCHAIN,
+    DIAMOND,
+    HELMET,
+    BOOK,
+    CROWN,
+    CHEST,
+    CANDLE_HOLDER,
+    MAP,
+    RING,
+
+    // movable cards
+    HOBBIT,
+    BAT,
+    GENIE,
+    GHOST,
+    DRAGON,
+    FAIRY,
+    MOUSE,
+    OWL,
+    SPIDER,
+    SKULL_MOTH,
+    SALAMANDER,
+    SCARAB
+}
